b6d01253ede086f9bba02179d564ea5b942e9402
Maciej Laskowski <maciek30088@gmail.com>
2023-06-16 21:17:15 +0100

    Add delete post feature

--- src/components/views/Post/Post.js
+++ src/components/views/Post/Post.js
@@ -1,13 +1,77 @@
-import styles from '../../common/PageTitle/PageTitle.module.scss';
-import PageTitle from '../../common/PageTitle/PageTitle';
+import { useSelector } from 'react-redux';
+import { getPostById } from '../../../redux/postsRedux';
+import { useParams, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import { Button } from 'react-bootstrap';
+import { removePost } from '../../../redux/postsRedux';
+import { useDispatch } from 'react-redux';
+import { Navigate } from 'react-router-dom';
 
 const Post = () => {
-  return (
-    <div className={styles.PageTitleContainer}>
-      <PageTitle>Post</PageTitle>
-      <p>lorem ipsum</p>
-    </div>
-  );
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const postData = useSelector((state) => getPostById(state, id));
+  const [showModal, setShowModal] = useState(false);
+
+  const handleRemovePost = () => {
+    setShowModal(true);
+  };
+
+  const handleCancel = () => {
+    setShowModal(false);
+  };
+
+  const handleDelete = () => {
+    dispatch(removePost(id));
+    setShowModal(false);
+    navigate('/');
+  };
+  if (!postData) return <Navigate to="/" />;
+  else
+    return (
+      <div>
+        <div className="d-flex justify-content-between my-2">
+          <h2>{postData.title}</h2>
+          <div>
+            <Link to={`/post/edit/${id}`} className="btn btn-outline-info mx-2">
+              Edit
+            </Link>
+            <Button variant="btn btn-outline-danger" onClick={handleRemovePost}>
+              Delete
+            </Button>
+          </div>
+        </div>
+        <div>
+          <p className="mb-0">
+            <span className="fw-bold">Author: </span>
+            {postData.author}
+          </p>
+          <p>
+            <span className="fw-bold">Published: </span>
+            {postData.publishedDate}
+          </p>
+          <p>{postData.content}</p>
+        </div>
+
+        <Modal show={showModal} onHide={handleCancel}>
+          <Modal.Header closeButton>
+            <Modal.Title>Confirm Delete</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Are you sure you want to delete this post?</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleCancel}>
+              Cancel
+            </Button>
+            <Button variant="danger" onClick={handleDelete}>
+              Delete
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
 };
 
 export default Post;
--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -1,11 +1,22 @@
+//variables
+const REMOVE_POST = 'app/posts/REMOVE_POST';
+
 //selectors
 export const getPosts = (state) => state.posts;
+export const getPostById = (state, postId) => {
+  return state.posts.find((post) => post.id === postId);
+}
 // actions
-// const createActionName = (actionName) => `app/posts/${actionName}`;
-
+//const createActionName = (actionName) => `app/posts/${actionName}`;
 // action creators
+export const removePost = (payload) => ({
+  type: REMOVE_POST,
+  payload
+})
 const postsReducer = (statePart = [], action) => {
   switch (action.type) {
+    case REMOVE_POST:
+      return statePart.filter((post) => post.id !== action.payload);
     default:
       return statePart;
   }
